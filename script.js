81c3484cee3ee59f87c3286e9c2936faa116c539
Kester <109535119+K3ster@users.noreply.github.com>
2024-10-03 07:35:47 +0000

    Lab 1

--- script.js
+++ script.js
@@ -14,5 +14,45 @@
     ex1_content.innerHTML = tabela.toString();
   };
 
-  
+  ex2_text.addEventListener("input", function () {
+    const input = this.value;
+    let message = "";
+
+    if (input.length > 9) {
+      message = "Długość numeru musi być równa 9";
+    } else if (/[a-zA-Z]/.test(input)) {
+      message = "Numer nie może zawierać liter";
+    } else if (/[^0-9]/.test(input)) {
+      message = "Numer nie może zawierać znaków specjalnych";
+    } else {
+      message = "Numer telefonu jest poprawny";
+    }
+
+    // Wyświetlanie komunikatu
+    ex2_content.textContent = message;
+  });
+
+  var ex3_one = document.getElementById("ex3_one");
+  var ex3_two = document.getElementById("ex3_two");
+  var ex3_element = document.getElementById("ex3_element");
+
+  ex3_element.addEventListener("dragstart", (e) => {
+    e.dataTransfer.setData("text/plain", e.target.id);
+  });
+
+  function handleDragOver(e) {
+    e.preventDefault();
+  }
+
+  function handleDrop(e) {
+    e.preventDefault();
+    const data = e.dataTransfer.getData("text/plain");
+    const draggableElement = document.getElementById(data);
+    e.target.appendChild(draggableElement);
+  }
+
+  [ex3_one, ex3_two].forEach((container) => {
+    container.addEventListener("dragover", handleDragOver);
+    container.addEventListener("drop", handleDrop);
+  });
 })();
