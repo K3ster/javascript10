bf141e9f24d45bcc6afc0638a2859522e6c67dbe
Kester <109535119+K3ster@users.noreply.github.com>
2024-10-03 07:21:55 +0000

    C1

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 (function () {
-  //TODO
+  var ex1_button = document.getElementById("ex1_button");
+
+  var ex1_content = document.getElementById("ex1_content");
+  var ex2_text = document.getElementById("ex2_text");
+  var ex2_content = document.getElementById("ex2_content");
+  ex1_button.onclick = function () {
+    var tabela = [];
+
+    for (var i = 0; i <= 9; i++) {
+      tabela.push(i);
+    }
+
+    ex1_content.innerHTML = tabela.toString();
+  };
+
+  
 })();
